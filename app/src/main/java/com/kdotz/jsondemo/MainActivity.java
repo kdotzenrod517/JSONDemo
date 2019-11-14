727d0ad1436f96f50fb394fe77c08716c560103e
Krista Dotzenrod <kdotzenrod517@gmail.com>
2019-11-13 21:10:15 -0600

    initial commit

---
+++ app/src/main/java/com/kdotz/jsondemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.kdotz.jsondemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22").get();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Log.i("Result", result);
+    }
+}
